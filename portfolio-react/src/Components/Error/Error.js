7374511dbf2df99637170ccb6274dabdcb2ae480
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-07-13 16:26:41 +0200

    Updates

--- portfolio-react/src/Components/Error/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Error = () => {
-    return (
-        <section className="my-work">
-            <h1>Error 404, page not found.</h1>
-        </section>  
-    )
-}
