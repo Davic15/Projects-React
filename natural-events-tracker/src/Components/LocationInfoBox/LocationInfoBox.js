78d372a70f6f82a02d0a620b9376eb1ef1d5ed2f
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-07-19 09:38:41 +0200

    Natural Events Tracker - React.js

--- natural-events-tracker/src/Components/LocationInfoBox/LocationInfoBox.js
+++ natural-events-tracker/src/Components/LocationInfoBox/LocationInfoBox.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './LocationInfoBox.module.css';
 
 export const LocationInfoBox = ({ info }) => {
+    const [isActive, setIsActive] = useState(false);
+    const handleActivation = () => {
+        setIsActive(false)
+    }
+
+    useEffect(() => {
+        setIsActive(true)
+    },[info.id])
 
     return (
-        <div className={styles["location-info"]} id='main'>
+        <div className={styles[`${isActive ? 'location-info' : 'location-info-hide'}`]}>
             <h1>Event Location Info</h1>
             <ul>
                 <li>ID: <strong>{ info.id }</strong></li>
                 <li>Title: <strong>{ info.title }</strong></li>
             </ul>
+            <button onClick={ handleActivation }>Close</button>
         </div>
     )
 }
