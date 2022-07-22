df47d505983c674b783c37afcf5675927f0c0fbc
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-07-22 16:32:25 +0200

    Dota 2 project React.js

--- dota2-app-react/src/Components/Pages/PlayerDetails/PlayerDetails.js
+++ dota2-app-react/src/Components/Pages/PlayerDetails/PlayerDetails.js
@@ -1,34 +1,12 @@
 import React from 'react';
+import { defaultPlayer } from '../../../data/defaultData';
 import styles from './PlayerDetails.module.css';
 
+
 export const PlayerDetails = (props) => {
-    const defaultPlayer = {
-        solo_competitive_rank: 5024,
-        profile: {
-            account_id: 125767523,
-            personaname: "BURN BABY BURN!!!",
-            name: "BURN BABY BURN!!!",
-            plus: false,
-            cheese: 0,
-            steamid: "76561198086033251",
-            avatar: "https://avatars.akamai.steamstatic.com/8b2f4c8e7734b057d5e469b31f33e933bd0934ec.jpg",
-            avatarmedium: "https://avatars.akamai.steamstatic.com/8b2f4c8e7734b057d5e469b31f33e933bd0934ec_medium.jpg",
-            avatarfull: "https://avatars.akamai.steamstatic.com/8b2f4c8e7734b057d5e469b31f33e933bd0934ec_full.jpg",
-            profileurl: "https://steamcommunity.com/id/kadiraktass/",
-            last_login: "2019-09-12T22:22:26.639Z",
-            loccountrycode: null,
-            status: null,
-            is_contributor: false,
-            is_subscriber: false
-        },
-        leaderboard_rank: null,
-        competitive_rank: 4952,
-        rank_tier: 80,
-        mmr_estimate: {
-            estimate: 4106
-        }
-    }
+
     const data = props.proPlayer || defaultPlayer;
+
     return (
         <div className={styles.container}>
             <h2>{data.profile.name}</h2>
