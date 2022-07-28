de503ea4cd354837185d28235f3b03717bbdaa3d
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-07-28 14:37:05 +0200

    Fixing GA

--- portfolio-react/src/useGATracker.js
+++ portfolio-react/src/useGATracker.js
@@ -8,14 +8,12 @@ const useGaTracker = () => {
         if (!window.location.href.includes("localhost")) {
             ReactGA.initialize("UA-235924979-1");
             setInitialized(true);
-            console.log('Initialized GA.')
         }
     }, []);
 
     useEffect(() => {
         if (initialized) {
             ReactGA.pageview(window.location.pathname + window.location.search);
-            console.log('Working GA.')
         }
     }, [initialized]);
 };
