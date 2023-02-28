da88a660dfe4049d3eaaed9f07805d51afd29e4c
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-03-01 00:42:38 +0100

    New Portfolio

---
+++ portfolio-react-v2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MyRoutes } from "./routes/MyRoutes";
+
+function App() {
+  return (
+    <div>
+      <MyRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-react-v2/src/routes/MyRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Banner } from '../components/content/Banner';
+import { Header } from '../components/layout/Header';
+
+export const MyRoutes = () => {
+    return (
+        <>
+            <Header />
+            <Banner />
+        </>
+    )
+}
