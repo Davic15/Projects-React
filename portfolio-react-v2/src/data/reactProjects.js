2fc4a717719827f4d96706cc0c986c9f1de79a39
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-03-09 20:39:12 +0100

    Updating portfolio

--- portfolio-react-v2/src/data/reactProjects.js
+++ portfolio-react-v2/src/data/reactProjects.js
@@ -1,24 +1,24 @@
 export const reactProjects = [
     {
         'id': '01',
-        'title': 'Note taking Application',
-        'description': 'Design & Development',
+        'title': 'Note Taking Application',
+        'description': 'Design, Development, React & Third party libraries',
         'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-07_ipq8mj.png',
         'github': 'https://github.com/Davic15/Projects-React/tree/main/notes-app-v2',
         'website': 'https://dapper-churros-b0843a.netlify.app/'
     },
     {
         'id': '02',
-        'title': 'Meme Generator',
-        'description': 'Design & Development',
-        'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688519/portfolio/portfolio-08_fcesly.png',
-        'github': 'https://github.com/Davic15/Projects-React/tree/main/meme-generator-v2',
-        'website': 'https://fascinating-seahorse-fdf5d6.netlify.app/'
+        'title': 'IP Address Tracker',
+        'description': 'Design, Development, React, Responsive, Leaflet map & API',
+        'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1678363188/portfolio/portfolio-15_x6dlw4.png',
+        'github': 'https://github.com/Davic15/frontendmentor/tree/main/ip-address-tracker',
+        'website': 'https://sparkling-taiyaki-df9264.netlify.app/'
     },
     {
         'id': '03',
-        'title': 'The Weather App',
-        'description': 'Design & Development',
+        'title': 'Weather App',
+        'description': 'Design, Development, React, Responsive & API',
         'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-03_ivsg2e.png',
         'github': 'https://github.com/Davic15/Projects-React/tree/main/weather-app-react',
         'website': 'https://stellular-paprenjak-9b8ba7.netlify.app/'
@@ -26,23 +26,15 @@ export const reactProjects = [
     {
         'id': '04',
         'title': 'Natural Events Tracker',
-        'description': 'Design & Development',
+        'description': 'Design, Development, React, Responsive, Leaflet map & API',
         'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688519/portfolio/portfolio-10_yys5di.png',
         'github': 'https://github.com/Davic15/Projects-React/tree/main/natural-events-tracker',
         'website': 'https://darling-frangollo-714c4a.netlify.app/'
     },
     {
         'id': '05',
-        'title': 'Dota 2',
-        'description': 'Design & Development',
-        'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-11_eh19fb.png',
-        'github': 'https://github.com/Davic15/Projects-React/tree/main/dota2-app-react',
-        'website': 'https://fantastic-hamster-709973.netlify.app/home'
-    },
-    {
-        'id': '06',
         'title': 'Pokedex',
-        'description': 'Design & Development',
+        'description': 'Design, Development, React, Responsive, Bootstrap & API',
         'imgUrl': 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-13_zwzym1.png',
         'github': 'https://github.com/Davic15/Projects-React/tree/main/pokedex',
         'website': 'https://delightful-strudel-61d976.netlify.app/'
