5e91e3a9c1c3d31c822e142b2d8f8fffe18bf9a7
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-05-27 01:08:10 +0200

    Added the section with my projects

--- portfolio-react-v3/src/components/content/CardProjects.jsx
+++ portfolio-react-v3/src/components/content/CardProjects.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Nav } from 'react-bootstrap';
+import { FaGithub } from 'react-icons/fa';
+import { AiFillChrome } from 'react-icons/ai';
+
+import './CardProjects.css';
 
 export const CardProjects = (props) => {
     console.log(props);
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem' }} className='main-card'>
             <Card.Img variant='top' src={props.imgUrl} />
-            <Card.Body>
+            <Card.Body className='main-card-body'>
                 <Card.Title>{props.title}</Card.Title>
-                <Card.Text>{props.description}</Card.Text>
-                <Button variant='primary'>Go somewhere</Button>
-                <Nav.Link href={props.website} variant='pill'>
-                    Website
+                <Card.Text className='card-text-custom'>
+                    {props.description}
+                </Card.Text>
+                <Nav.Link
+                    href={props.github}
+                    target='_blank'
+                    className='btn-custom_new'
+                >
+                    <FaGithub />
+                </Nav.Link>
+                <Nav.Link
+                    href={props.website}
+                    target='_blank'
+                    className='btn-custom_new'
+                >
+                    <AiFillChrome />
                 </Nav.Link>
             </Card.Body>
         </Card>
--- portfolio-react-v3/src/data/projects.js
+++ portfolio-react-v3/src/data/projects.js
@@ -1,8 +1,9 @@
 export const projects = [
-    {
+    /*{
         id: '01',
         title: 'Dad Jokes API',
-        description: 'Design, Development, JavaScript & API',
+        description:
+            'HTML, CSS, JavaScript, XMLHttpRequest and Rest API (Dad Jokes).',
         imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688517/portfolio/portfolio-02_ogjmkd.png',
         github: 'https://github.com/Davic15/Projects-JavaScript-Vanilla/tree/main/Project-06-DadJokesAPI',
         website: 'https://remarkable-starship-4ec224.netlify.app/',
@@ -10,7 +11,8 @@ export const projects = [
     {
         id: '02',
         title: 'Weather App',
-        description: 'Design, Development, JavaScript & API',
+        description:
+            'HTML, CSS, JavaScript, JQuery and Rest API (OpenWeatherMap).',
         imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-03_ivsg2e.png',
         github: 'https://github.com/Davic15/Projects-JavaScript-Vanilla/tree/main/Project-06-WeatherApp',
         website: 'https://deft-lolly-86fd47.netlify.app/',
@@ -18,9 +20,72 @@ export const projects = [
     {
         id: '03',
         title: 'Wikipedia API',
-        description: 'Design, Development, JavaScript & API',
+        description: 'HTML, CSS, JavaScript, Axios, Rest API (Wikipedia).',
         imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-05_l0blkd.png',
         github: 'https://github.com/Davic15/Projects-JavaScript-Vanilla/tree/main/Project-08-WikipediaAPI',
         website: 'https://peppy-sable-3b367f.netlify.app/',
+    },
+    {
+        id: '04',
+        title: 'Note Taking Application',
+        description:
+            'HTML, CSS, JavaScript, React.js, showdown, react-split, react-mde, nanoid and localStorage.',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-07_ipq8mj.png',
+        github: 'https://github.com/Davic15/Projects-React/tree/main/notes-app-v2',
+        website: 'https://dapper-churros-b0843a.netlify.app/',
+    },*/
+    {
+        id: '05',
+        title: 'IP Address Tracker',
+        description:
+            'HTML, CSS, JavaScript, React.js, Leaflet map and Rest API (ipify).',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1678363188/portfolio/portfolio-15_x6dlw4.png',
+        github: 'https://github.com/Davic15/frontendmentor/tree/main/ip-address-tracker',
+        website: 'https://sparkling-taiyaki-df9264.netlify.app/',
+    },
+    {
+        id: '06',
+        title: 'Weather App',
+        description:
+            'HTML, CSS, React.js, Bootstrap and Rest API (OpenWeatherMap).',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-03_ivsg2e.png',
+        github: 'https://github.com/Davic15/Projects-React/tree/main/weather-app-react',
+        website: 'https://stellular-paprenjak-9b8ba7.netlify.app/',
+    },
+    {
+        id: '07',
+        title: 'Natural Events Tracker',
+        description:
+            'HTML, CSS, JavaScript, React.js, Pidgeon map and Rest API (EONET).',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688519/portfolio/portfolio-10_yys5di.png',
+        github: 'https://github.com/Davic15/Projects-React/tree/main/natural-events-tracker',
+        website: 'https://darling-frangollo-714c4a.netlify.app/',
+    },
+    {
+        id: '08',
+        title: 'Pokedex',
+        description:
+            'HTML, CSS, JavaScript, React.js, Bootstrap, React Loader and Rest API (PokeAPI).',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677688518/portfolio/portfolio-13_zwzym1.png',
+        github: 'https://github.com/Davic15/Projects-React/tree/main/pokedex',
+        website: 'https://delightful-strudel-61d976.netlify.app/',
+    },
+    {
+        id: '09',
+        title: 'Online Show',
+        description:
+            'HTML, CSS, JavaScript, React.js, Redux (toolkit), Express.js, Node.js, Rest API.',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1677799466/portfolio/portfolio-14_or5az3.png',
+        github: 'https://github.com/Davic15/Projects-React/tree/main/online-shop',
+        website: 'https://resplendent-souffle-6e7f71.netlify.app/',
+    },
+    {
+        id: '10',
+        title: 'Rest Country API',
+        description:
+            'HTML, CSS, JavaScript, React.js, Redux.js, Rest API (restcountries).',
+        imgUrl: 'https://res.cloudinary.com/dccsb07gm/image/upload/v1681587832/portfolio/portfolio-16_q1cqa8.png',
+        github: 'https://github.com/Davic15/frontendmentor/tree/main/rest-countries-api',
+        website: 'https://earnest-blini-cb1afa.netlify.app/',
     },
 ];
